59c1c2baf7f3c4ed3dffef6dae4897716893a01a
YXL <chenxin.lan.76@gmail.com>
2019-06-27 16:10:10 +0800

    get fans url

---
+++ get_fans.py
@@ -0,0 +1,51 @@
+import requests
+from time import sleep
+from bs4 import BeautifulSoup
+
+from settings import headers, target_user, fans_file
+
+session = requests.Session()
+url = 'https://www.douban.com/people/' + target_user + '/rev_contacts'
+
+
+# 获取友邻数
+def get_num(soup):
+    num = soup.select('#db-usr-profile > div.info > h1')
+    num = BeautifulSoup(str(num[0]), 'lxml').string
+    length = len(num) - 2
+    while '0' < num[length] < '9':
+        length -= 1
+    return int(num[length + 1:len(num) - 1])
+
+
+def write_to_file(num):
+    file = open(fans_file, "w")
+    # 遍历所有友邻页面
+    for i in range(0, num, 70):
+        current_url = url + '?start=' + str(i)
+        # print(current_url)
+        response = session.get(url=current_url, headers=headers)
+        sleep(2)
+        soup = BeautifulSoup(response.text, 'lxml')
+        peoples = soup.select('#content > div > div.article > dl > dt > a')
+        for people in peoples:
+            # print(BeautifulSoup(str(people), 'lxml').a['href'])
+            # 将各个友邻的主页地址写入文件
+            file.write(BeautifulSoup(str(people), 'lxml').a['href'] + '\n')
+    file.close()
+
+
+def get_fans():
+    # 目标用户友邻列表
+    response = session.get(url=url, headers=headers)
+    if response.status_code != 200:
+        print('获取失败')
+        return False
+    print('获取成功')
+    sleep(2)
+    soup = BeautifulSoup(response.text, 'lxml')
+    # print(soup)
+    num = get_num(soup)
+    # print(num)
+    write_to_file(num)
+    return True
---
+++ settings.py
@@ -0,0 +1,19 @@
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like     Chrome/67.0.3396.62 "
+                  "Safari/537.36 ",
+    'Cookie': 'bid=F4TFvq5FREI; __yadk_uid=UqOpneRFFppArLSm1VOq0USJ9ESuSToY; push_doumail_num=0; '
+              '__utmv=30149280.15173; douban-fav-remind=1; douban-profile-remind=1; '
+              '_vwo_uuid_v2=DF9F566BC74313DEBD9AC897D3A6B637F|4e76902c5867ac895d0884a33321d98d; '
+              '__utmz=30149280.1552018981.6.4.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=('
+              'not%20provided); __utma=30149280.392583861.1550463445.1552903717.1552906732.11; viewed="3872870"; '
+              'gr_user_id=714a0e10-08db-4d80-842e-628f26064f70; push_noty_num=0; ct=y; ll="108288"; '
+              '_pk_ref.100001.8cb4=%5B%22%22%2C%22%22%2C1561603607%2C%22https%3A%2F%2Faccounts.douban.com'
+              '%2Fpassport%2Flogin_popup%3Flogin_source%3Danony%22%5D; _pk_ses.100001.8cb4=*; ap_v=0,'
+              '6.0; _ga=GA1.2.392583861.1550463445; _gid=GA1.2.11423082.1561603834; '
+              'dbcl2="151739065:V3chgK6X8ko"; ck=uBQq; '
+              '_pk_id.100001.8cb4=b3f6aba4efec8d14.1550463444.44.1561608110.1561569458. '
+}
+
+target_user = 'zeqingg'
+
+fans_file = 'fans_url.txt'
