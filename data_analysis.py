aee23f0c96f556733a93bfe9d0fe8a7175946a76
YXL <chenxin.lan.76@gmail.com>
2019-06-30 15:52:33 +0800

    notebook

--- data_analysis.py
+++ data_analysis.py
@@ -3,17 +3,28 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
-from settings import dataset_file, follower_range, reg_year_range, gender_range
+from settings import dataset_file
+from settings import follower_range
+from settings import reg_year_range
+from settings import gender_range
+from settings import loc_range
+from settings import loc_id
+from settings import loc_dic
 
 matplotlib.rcParams['font.sans-serif'] = ['SimHei']
 
-follower_num = list(0 for fr in range(len(follower_range)))
-reg_year_num = list(0 for rr in range(len(reg_year_range)))
-gender_num = [0, 0, 0]
+follower_num = list(0 for fi in range(len(follower_range)))
+loc_num = list(0 for li in range(len(loc_range)))
+reg_year_num = list(0 for ri in range(len(reg_year_range)))
+gender_num = list(0 for gi in range(len(gender_range)))
 
 
 def draw_bar_chart(label, num):
     ind = np.arange(len(label))
+    index = 0
+    while index < len(ind):
+        ind[index] *= 6
+        index += 1
     top = int(max(num) * 1.2)
 
     rects = plt.bar(ind, num, 0.4, color='#FFC46C')
@@ -36,15 +47,6 @@ def draw_pie_chart(label, num):
     plt.show()
 
 
-def gender_analysis(item):
-    if item == 'M':
-        gender_num[0] += 1
-    elif item == 'F':
-        gender_num[1] += 1
-    else:
-        gender_num[2] += 1
-
-
 def item_analysis(item, item_range, item_num):
     index = 0
     while index <= len(item_range):
@@ -56,6 +58,26 @@ def item_analysis(item, item_range, item_num):
     # print(item_num)
 
 
+def loc_analysis(item):
+    if item == '':
+        loc_num[loc_id['未知']] += 1
+        return
+    loc = loc_dic.get(item)
+    if loc is None:
+        loc_num[loc_id['国外']] += 1
+    else:
+        loc_num[loc_id[loc]] += 1
+
+
+def gender_analysis(item):
+    if item == 'M':
+        gender_num[0] += 1
+    elif item == 'F':
+        gender_num[1] += 1
+    else:
+        gender_num[2] += 1
+
+
 def read_dataset_file():
     outfile = open(dataset_file, 'r', encoding='utf-8')
     csv_file = csv.reader(outfile)
@@ -65,6 +87,7 @@ def read_dataset_file():
             continue
         # print(item)
         item_analysis(int(item[0]), follower_range, follower_num)
+        loc_analysis(item[1])
         item_analysis(int(item[3]), reg_year_range, reg_year_num)
         gender_analysis(item[4])
 
@@ -76,6 +99,7 @@ def read_dataset_file():
     label.append(str(follower_range[-1]) + '+')
     # print(label)
     draw_bar_chart(label, follower_num)
+    draw_bar_chart(loc_range, loc_num)
     draw_pie_chart(reg_year_range, reg_year_num)
     draw_pie_chart(gender_range, gender_num)
 
